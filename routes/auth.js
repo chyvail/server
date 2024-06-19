6a3ad761d6798ec7537ff86c8ae0beca5765acbb
joshua chivile <joshuachivile@gmail.com>
2024-06-19 15:27:39 +0300

    Comments

--- routes/auth.js
+++ routes/auth.js
@@ -66,17 +66,19 @@ router.post("/register", upload.single("profileImage"), async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }); // find user based on email
     if (!user) {
       return res.status(409).json({ message: "User doesn't exists" });
     }
-    // password check
+    // password check with bcrypt
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid Credentials" });
     }
     // Token Generation
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); // sign id as users id
+
+    // delete password from user object returned
     const userObject = user.toObject();
     delete userObject.password;
     res.status(200).json({ token, userObject });
